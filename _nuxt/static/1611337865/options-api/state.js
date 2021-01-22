2b46eb2bcd8b7a6296892e3013c5353a3a41c25d
soerenmartius <soerenmartius@users.noreply.github.com>
2021-01-22 17:51:07 +0000

    deploy: 4f405479e9f980b21207c7e37afd8bafae36680e

---
+++ _nuxt/static/1611337865/options-api/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611337865",layout:"default",error:null,state:{description:"I'm defined by server side",setting:{darkMode:false}},serverRendered:true,routePath:"\u002Foptions-api",config:{},globalRefs:{}};
