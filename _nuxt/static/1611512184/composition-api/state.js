32de5905432945703baa35efda7a2997ef3c4d46
soerenmartius <soerenmartius@users.noreply.github.com>
2021-01-24 18:16:26 +0000

    deploy: 26edb10f5164cbc762168937927f3087bc7d6471

---
+++ _nuxt/static/1611512184/composition-api/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1611512184",layout:"default",error:null,state:{description:"I'm defined by server side",setting:{darkMode:false}},serverRendered:true,routePath:"\u002Fcomposition-api",config:{},globalRefs:{},ssrRefs:{"IRikcoqVLdioWzCsVeVJHw==":"I'm defined on asyncData()","0eQhp2akus9xxdtNwV1t5Q==":"Unknown"}};
