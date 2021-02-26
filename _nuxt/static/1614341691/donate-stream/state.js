2ea9f45b55c5171c193b86790fc279a7729ce300
soerenmartius <soerenmartius@users.noreply.github.com>
2021-02-26 18:15:57 +0000

    deploy: 9e0f31dc3376c52ebeca71930d1870911ee31de8

--- _nuxt/static/1614341691/donate-stream/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614341691",layout:"default",error:null,state:{description:"I'm defined by server side",setting:{darkMode:false}},serverRendered:true,routePath:"\u002Fdonate-stream",config:{content:{dbHash:"de1fd95e"}},globalRefs:{}};
