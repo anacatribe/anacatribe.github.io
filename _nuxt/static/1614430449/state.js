2ac35417eead0b817b60ba65a015792909aca1fd
soerenmartius <soerenmartius@users.noreply.github.com>
2021-02-27 12:54:10 +0000

    deploy: fb59c0721fe3b13e94953d8824d8f674b20797d5

---
+++ _nuxt/static/1614430449/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614430449",layout:"default",error:null,state:{description:"I'm defined by server side",setting:{darkMode:false}},serverRendered:true,routePath:"\u002F",config:{content:{dbHash:"0f78502c"}},globalRefs:{}};
