a17940f7a2aa6f57e540337d2a7faac0e2687801
Soren Martius <soeren.martius@gmail.com>
2021-03-24 00:09:32 +0100

    feat: add artist overview

---
+++ components/artist.vue
@@ -0,0 +1,140 @@
+<template>
+  <div
+    class="space-y-4 sm:grid sm:grid-cols-3 sm:items-start sm:gap-6 sm:space-y-0"
+  >
+    <div class="aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
+      <img
+        class="object-cover rounded-lg shadow-lg"
+        :src="require(`~/assets/img/artists/${image}`)"
+        alt="Press Photo of DJ Lica"
+      >
+    </div>
+    <div class="sm:col-span-2">
+      <div class="space-y-4">
+        <div class="space-y-1 font-medium leading-6">
+          <h3 class="text-xl">
+            {{ name }}
+          </h3>
+          <p class="text-lg text-gray-400">
+            {{ location }}
+          </p>
+        </div>
+        <div class="text-lg">
+          <p class="text-gray-500">
+            <slot />
+          </p>
+        </div>
+        <ul class="flex space-x-5">
+          <li>
+            <a
+              :href="soundcloud"
+              target="_blank"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">Soundcloud</span>
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5"
+                fill="currentColor"
+              >
+                <path
+                  d="m20.25 11c-.267 0-.529.029-.782.082-.337-3.409-3.222-6.082-6.718-6.082-.414 0-.75.336-.75.75v12c0 .414.336.75.75.75h7.5c2.069 0 3.75-1.681 3.75-3.75 0-2.067-1.681-3.75-3.75-3.75z"
+                />
+                <path
+                  d="m9 7.25v10.5c0 .414.336.75.75.75s.75-.336.75-.75v-10.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+                />
+                <path
+                  d="m6 10.25v7.5c0 .414.336.75.75.75s.75-.336.75-.75v-7.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+                />
+                <path
+                  d="m3 17.75c0 .414.336.75.75.75s.75-.336.75-.75v-7.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
+                />
+                <path
+                  d="m.75 18.5c.414 0 .75-.336.75-.75v-4.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75v4.5c0 .414.336.75.75.75z"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              :href="facebook"
+              target="_blank"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">Facebook</span>
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5"
+                fill="currentColor"
+              >
+                <path
+                  d="m15.997 3.985h2.191v-3.816c-.378-.052-1.678-.169-3.192-.169-3.159 0-5.323 1.987-5.323 5.639v3.361h-3.486v4.266h3.486v10.734h4.274v-10.733h3.345l.531-4.266h-3.877v-2.939c.001-1.233.333-2.077 2.051-2.077z"
+                />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a
+              :href="instagram"
+              target="_blank"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">Instagram</span>
+              <svg
+                viewBox="0 0 512 512"
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5"
+                fill="currentColor"
+              >
+                <path
+                  d="m301 256c0 24.851562-20.148438 45-45 45s-45-20.148438-45-45 20.148438-45 45-45 45 20.148438 45 45zm0 0"
+                />
+                <path
+                  d="m332 120h-152c-33.085938 0-60 26.914062-60 60v152c0 33.085938 26.914062 60 60 60h152c33.085938 0 60-26.914062 60-60v-152c0-33.085938-26.914062-60-60-60zm-76 211c-41.355469 0-75-33.644531-75-75s33.644531-75 75-75 75 33.644531 75 75-33.644531 75-75 75zm86-146c-8.285156 0-15-6.714844-15-15s6.714844-15 15-15 15 6.714844 15 15-6.714844 15-15 15zm0 0"
+                />
+                <path
+                  d="m377 0h-242c-74.4375 0-135 60.5625-135 135v242c0 74.4375 60.5625 135 135 135h242c74.4375 0 135-60.5625 135-135v-242c0-74.4375-60.5625-135-135-135zm45 332c0 49.625-40.375 90-90 90h-152c-49.625 0-90-40.375-90-90v-152c0-49.625 40.375-90 90-90h152c49.625 0 90 40.375 90 90zm0 0"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    soundcloud: {
+      type: String,
+      required: true
+    },
+    facebook: {
+      type: String,
+      required: true
+    },
+    instagram: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
---
+++ pages/artists.vue
@@ -0,0 +1,69 @@
+<template>
+  <main>
+    <section id="artists" class="pt-16">
+      <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <div class="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
+          <div class="space-y-5 sm:space-y-4">
+            <h2
+              class="text-3xl font-extrabold tracking-tight text-center sm:text-4xl sm:text-left"
+            >
+              Artists
+            </h2>
+            <p class="text-xl text-center text-gray-500 sm:text-left">
+              Meet our international community of artists that share the same
+              passion for electronic music production. We frequently add new
+              artists to our tribe and provide you with exclusive content for
+              the anaca tribe community.
+            </p>
+          </div>
+          <div class="lg:col-span-2">
+            <ul
+              class="space-y-12 sm:divide-y sm:divide-gray-200 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0"
+            >
+              <li class="sm:py-8">
+                <artist
+                  name="Lica"
+                  location="London"
+                  soundcloud="https://soundcloud.com/ilonalica"
+                  facebook="https://www.facebook.com/licaofficial/about/"
+                  instagram="https://www.instagram.com/ilona.lica"
+                  image="lica.webp"
+                >
+                  Estonian born, Ilona aka LICA has enjoyed a rapid ascent
+                  through the Electronic music scene with her energetic and
+                  mesmerising sound inspired by a spectrum of genres and
+                  diversity of tempo. Touching the decks for the first time 10
+                  years ago in her home country, LICA became well-known on the
+                  Drum’n’Bass underground music scene orchestrating events and
+                  projects.
+                </artist>
+              </li>
+              <li class="sm:py-8">
+                <artist
+                  name="Ana"
+                  location="Sao Paulo"
+                  soundcloud="https://soundcloud.app.goo.gl/BreCFfowxTMNJtm4A"
+                  facebook="https://www.facebook.com/licaofficial/about/"
+                  instagram="https://www.instagram.com/ilona.lica"
+                  image="ana.webp"
+                >
+                  Elegance. Word that describes the melody within Ana
+                  Jablonski’s sound. The Brazilian artist started her career in
+                  2016. Jablonski fell under the spell of house music at a young
+                  age though her father, a music enthusiast. She captivates the
+                  scene with her performance, versatile sound and dazzles the
+                  club and fashion circuit in Brazil. Ana worked as a model for
+                  ten years, though time, the young artist collected experience
+                  all over the world, getting to know different cultures,
+                  impacting her identity. Those years contributed to her
+                  knowledge in music, becoming a DJ with good references and a
+                  strong personality.
+                </artist>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
