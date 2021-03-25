9522c7067f3bf41c6baa668bf1bb8a1557d3c3d1
soerenmartius <soerenmartius@users.noreply.github.com>
2021-03-25 08:44:34 +0000

    deploy: 6a83fbed9faf7cbbffca2f816549dec42b7b3ceb

--- _nuxt/static/1616661231/dataprivacy/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616661231",layout:"default",error:null,state:{description:"I'm defined by server side",setting:{darkMode:false}},serverRendered:true,routePath:"\u002Fdataprivacy",config:{content:{dbHash:"78803073"}},globalRefs:{}};
